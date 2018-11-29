08f9dd66b82b95aa824c8d416c94e036515399ba
luanmotta <luanmotta97@hotmail.com>
2018-11-29 11:08:51 -0200

    Update filmes

--- cliente/mostra.php
+++ cliente/mostra.php
@@ -15,28 +15,32 @@
     <?php
     require("../conecta.inc.php");
 
-    $queryEstados = "
+    $queryClientes = "
       SELECT
-        estadoId,
-        nome
-      FROM estado
+        id,
+        nome,
+        endereco
+      FROM cliente
     ;";
 
     $ok = conecta_bd() or die ("Nao foi possivel conectar-se ao servidor.");
-    $estados = mysqli_query($ok, $queryEstados) or die ("Nao foi possivel consultar estados.");
+    $clientes = mysqli_query($ok, $queryClientes) or die ("Nao foi possivel consultar clientes.");
 
-    print("<center><h2>Estados</h2>");
+    print("<center><h2>Clientes</h2>");
     print("<table border='1' bordercolor='red'>");
     print("<tr><td><b>Id</td>");
     print("<td><b>Nome</td>");
+    print("<td><b>Endereco</td>");
     print("<td><b>Deletar</td><td><b>Alterar</td></tr>");
 
-    while ($row = mysqli_fetch_array($estados)) {
-       $id = $row["estadoId"];
-       $estado = $row["nome"];
+    while ($row = mysqli_fetch_array($clientes)) {
+       $id = $row["clienteId"];
+       $nome = $row["nome"];
+       $endereco = $row["endereco"];
 
        print("<tr><td>$id</td>");
-       print("<td>$estado</td>");
+       print("<td>$nome</td>");
+       print("<td>$endereco</td>");
        print("<td><a href='deletar.php?id=$id'>Deletar</a></td>");
        print("<td><a href='alterar.php?id=$id'>Alterar</a></td></tr>");
     }
@@ -44,7 +48,7 @@
 
     ?>
     <hr>
-    <a href="inserir.php"><button>Inserir Estado</button></a>
+    <a href="inserir.php"><button>Inserir Cliente</button></a>
     <p><a href="../index.php">Voltar</a></p>
   </body>
 </html>
--- filme/mostra.php
+++ filme/mostra.php
@@ -15,32 +15,32 @@
     <?php
     require("../conecta.inc.php");
 
-    $queryClientes = "
+    $queryFilmes = "
       SELECT
-        id,
+        filmeId,
         nome,
-        endereco
-      FROM cliente
+        valor
+      FROM filme
     ;";
 
     $ok = conecta_bd() or die ("Nao foi possivel conectar-se ao servidor.");
-    $clientes = mysqli_query($ok, $queryClientes) or die ("Nao foi possivel consultar clientes.");
+    $filmes = mysqli_query($ok, $queryFilmes) or die ("Nao foi possivel consultar filmes.");
 
-    print("<center><h2>Clientes</h2>");
+    print("<center><h2>Filmes</h2>");
     print("<table border='1' bordercolor='red'>");
     print("<tr><td><b>Id</td>");
     print("<td><b>Nome</td>");
-    print("<td><b>Endereco</td>");
+    print("<td><b>Valor</td>");
     print("<td><b>Deletar</td><td><b>Alterar</td></tr>");
 
-    while ($row = mysqli_fetch_array($clientes)) {
+    while ($row = mysqli_fetch_array($filmes)) {
        $id = $row["clienteId"];
        $nome = $row["nome"];
-       $endereco = $row["endereco"];
+       $valor = $row["valor"];
 
        print("<tr><td>$id</td>");
        print("<td>$nome</td>");
-       print("<td>$endereco</td>");
+       print("<td>$valor</td>");
        print("<td><a href='deletar.php?id=$id'>Deletar</a></td>");
        print("<td><a href='alterar.php?id=$id'>Alterar</a></td></tr>");
     }
@@ -48,7 +48,7 @@
 
     ?>
     <hr>
-    <a href="inserir.php"><button>Inserir Cliente</button></a>
+    <a href="inserir.php"><button>Inserir Filme</button></a>
     <p><a href="../index.php">Voltar</a></p>
   </body>
 </html>
